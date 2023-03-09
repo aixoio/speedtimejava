e996fafcb1ae026459100b12326303ad7e3ea015
aixoio <79969530+aixoio@users.noreply.github.com>
2023-03-09 18:58:37 +1100

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,51 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        System.out.println("Hello World!");
+        if (args.length <= 0) {
+
+            System.out.println("Usage: <command>");
+            System.exit(0);
+
+        }
+
+        System.out.println("\nStarting...\n");
+
+        Date starttime = new Date();
+
+        Main.runCommand(args[0]);
+
+        Date endtime = new Date();
+
+        System.out.println("\nEnding...\n");
+
+        long time = endtime.getTime() - starttime.getTime();
+
+        System.out.println(String.format("Commmand ran in %s ms!", time));
+        System.out.println(String.format("Start time: %s", starttime.getTime()));
+        System.out.println(String.format("End time: %s", endtime.getTime()));
+
+    }
+
+    private static void runCommand(String command) throws IOException, InterruptedException {
+
+        Process proc = Runtime.getRuntime().exec(command);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            System.out.print(line + "\n");
+        }
+
+        proc.waitFor();
 
     }
 
